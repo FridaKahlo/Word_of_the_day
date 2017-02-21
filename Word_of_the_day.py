b1f16048cf26325a8965e27d11314e1b1615c83d
laila <laila@fake.com>
2017-02-21 23:33:37 +0200

    Url/Saving image functions

--- Word_of_the_day.py
+++ Word_of_the_day.py
@@ -1,7 +1,31 @@
 import requests, json
-response = requests.request('GET','http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
-response_json = response.json()
-print(response_json)
+def main():
+    jum_de_url = half_url_image()
+    url_image = whole_url(jum_de_url)
+    image = save_image(url_image)
 
-url_image = response_json['images'][0]['url']
-print(url_image)
+def half_url_image():
+    response = requests.request('GET','http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
+    response_json = response.json()
+    print(response_json)
+
+    url_image = response_json['images'][0]['url']
+    print(url_image)
+    return url_image
+
+def whole_url(jum_de_url):
+    first_half_url= 'http://www.bing.com/'
+    whole_url = first_half_url+jum_de_url
+    print (whole_url)
+    return whole_url
+
+def save_image(url_image):
+    response = requests.get(url_image)
+    image_data = response.content
+    f = open('D:/Programming/Word_of_the_day/image.jpg','wb')
+    f.write(image_data)
+    f.close()
+
+
+if __name__ == '__main__':
+    main()

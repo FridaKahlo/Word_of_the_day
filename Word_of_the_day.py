4d3c8f0d46426f0bbe4a745365e14eb4eef1961f
laila <laila@fake.com>
2017-02-25 18:30:34 +0200

    Set wallpaper

--- Word_of_the_day.py
+++ Word_of_the_day.py
@@ -1,8 +1,18 @@
 import requests, json
+import ctypes
+
 def main():
     jum_de_url = half_url_image()
     url_image = whole_url(jum_de_url)
-    image = save_image(url_image)
+    image_path = save_image(url_image)
+    set_wallpaper(image_path)
+
+def set_wallpaper(image_path):
+    '''Change the Windows wallpaper to the image given by its path.
+    :param image_path: str, path to image, e.g. "C:/photos/image.jpg"'''
+    SPI_SETDESKWALLPAPER = 20
+    result = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
+    return result
 
 def half_url_image():
     response = requests.request('GET','http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
@@ -22,9 +32,16 @@ def whole_url(jum_de_url):
 def save_image(url_image):
     response = requests.get(url_image)
     image_data = response.content
-    f = open('D:/Programming/Word_of_the_day/image.jpg','wb')
+    x = url_image.split("/")
+    print(x)
+    #print (type (x))
+    z=x[-1]
+    print(z)
+    target_path = 'D:/Programming/Word_of_the_day/'+z
+    f = open(target_path,'wb')
     f.write(image_data)
     f.close()
+    return target_path
 
 
 if __name__ == '__main__':

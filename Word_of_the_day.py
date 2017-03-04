f040e59b71a01a4f93e4134731454b2c80c37c0b
laila <laila@fake.com>
2017-03-04 22:35:17 +0200

    Added docstrings

--- Word_of_the_day.py
+++ Word_of_the_day.py
@@ -20,6 +20,10 @@ def main():
     log.debug("Wallpaper set :%s",status)
 
 def random_quote():
+    """
+    Extract a random quote from brainyquote.com
+    :returns: quote and author from a site
+    """
     response_quote_page = requests.get('https://www.brainyquote.com/api/rand_q?langc=en')
     response_json_quote_page = response_quote_page.json()
     quote = response_json_quote_page["qt"]
@@ -27,13 +31,18 @@ def random_quote():
     return quote, author
 
 def set_wallpaper(image_path):
-    '''Change the Windows wallpaper to the image given by its path.
-    :param image_path: str, path to image, e.g. "C:/photos/image.jpg"'''
+    """Change the Windows wallpaper to the image given by its path.
+    :param image_path: the path of the image that should be set as a wallpaper, e.g. "C:/photos/image.jpg
+    :returns: the status code of the ctypes.windll.user32.SystemParametersInfoW function
+    """
     SPI_SETDESKWALLPAPER = 20
     result = ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image_path, 3)
     return result
 
 def half_url_image():
+    """
+    :returns: the half url of the image of the day which includes the name of the image
+    """
     response = requests.request('GET','http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US')
     response_json = response.json()
     log.debug("Type of variable is: %s",type(response_json))
@@ -43,7 +52,12 @@ def half_url_image():
     #print(url_image)
     return url_image
 
+
 def whole_url(jum_de_url):
+    """
+    :param jum_de_url: the half url of the image of the day which includes the name of the image
+    :returns: the whole url of the image of the day
+    """
     first_half_url= 'http://www.bing.com/'
     whole_url = first_half_url+jum_de_url
     log.debug("Whole_url: %s",whole_url)
@@ -51,27 +65,39 @@ def whole_url(jum_de_url):
     return whole_url
 
 def save_image(url_image):
+    """
+    :param url_image: the whole url of the image of the day
+    :returns: the path of the saved image
+    """
     response = requests.get(url_image)
     image_data = response.content
-    x = url_image.split("/")
-    log.debug("Split url : %s", x)
+    split_url = url_image.split("/")
+    log.debug("Split url : %s", split_url)
     #print (type (x))
-    z=x[-1]
+    name_of_image = split_url[-1]
     #print(z)
-    target_path = 'D:/Programming/Word_of_the_day/'+z
+    target_path = 'D:/Programming/Word_of_the_day/'+name_of_image
     f = open(target_path,'wb')
     f.write(image_data)
     f.close()
     return target_path
 
-def text_on_image(image_path,formatted_quote,coordonates,size):
+def text_on_image(image_path,formatted_quote,coordinates,size):
+    """
+    :param image_path: the path of the saved image of the day
+    :param formatted_quote: the quote and the author formatted to go to the next line after 50 characters
+    :param coordinates: the coordinates (x, y) of the text on the image
+    :param size: the size of the text on the image
+    :returns path_image_with_text: the path of the image with the quote and author on it
+    """
     desen = Image.open(image_path)
     experiment = ImageDraw.Draw(desen)
     font = ImageFont.truetype('calibri.ttf', size)
-    experiment.text(coordonates, formatted_quote, (255,0,0),font=font)
+    experiment.text(coordinates, formatted_quote, (255,0,0),font=font)
     path_image_with_text = 'd:/programming/prived-medved.jpg'
     desen.save(path_image_with_text)
     return path_image_with_text
+
 
 if __name__ == '__main__':
 
